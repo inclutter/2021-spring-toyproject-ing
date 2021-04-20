d7de2833795a2e5c5be05ba4922f49a866d77348
mwkang <augustman@knou.ac.kr>
2021-04-20 12:50:53 +0900

    jasypt dependency addtion.

--- src/main/java/com/study/toypro/ToyproApplication.java
+++ src/main/java/com/study/toypro/ToyproApplication.java
@@ -1,9 +1,11 @@
 package com.study.toypro;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class ToyproApplication {
 
     public static void main(String[] args) {
