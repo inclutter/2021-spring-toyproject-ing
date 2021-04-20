fcaeabea9dc9e7aa3b2db8d9361945abd865aca7
mwkang <augustman@knou.ac.kr>
2021-04-20 22:59:09 +0900

    datasource password encryption by jasypt

--- src/test/java/com/study/toypro/ToyproApplicationTests.java
+++ src/test/java/com/study/toypro/ToyproApplicationTests.java
@@ -1,13 +1,27 @@
 package com.study.toypro;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
 class ToyproApplicationTests {
 
     @Test
     void contextLoads() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword("m2"); // 암호화 Key 정보
+        encryptor.setAlgorithm("PBEWithMD5AndDES"); // 암호화 알고리즘
+        encryptor.setStringOutputType("base64");
+
+        String encryptedText = encryptor.encrypt("1qaz2wsx");
+        String decrypedText = encryptor.decrypt(encryptedText);
+
+        System.out.println("encryptedText : " + encryptedText);
+        System.out.println("decrypedText : " + decrypedText);
+
     }
 
 }
