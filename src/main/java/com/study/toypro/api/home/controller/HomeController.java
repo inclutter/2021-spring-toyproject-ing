7d64e086b71e633ee9a8a1da6116768e082433b4
mwkang <augustman@knou.ac.kr>
2021-05-26 23:53:23 +0900

    Have added about connection manager configuration.

--- src/main/java/com/study/toypro/api/home/controller/HomeController.java
+++ src/main/java/com/study/toypro/api/home/controller/HomeController.java
@@ -1,6 +1,6 @@
 package com.study.toypro.api.home.controller;
 
-import com.study.toypro.api.home.model.BoardVO;
+import com.study.toypro.api.home.dto.BoardDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,35 +22,35 @@ public class HomeController {
     }
 
     @GetMapping("/home/getBoardList")
-    public List<BoardVO> getBoardList() {
-        List<BoardVO> boardVOArrayList = new ArrayList<>();
+    public List<BoardDto> getBoardList() {
+        List<BoardDto> boardDtoArrayList = new ArrayList<>();
         for(int i = 1; i <= 10; i++) {
-            BoardVO boardVO = new BoardVO();
-            boardVO.setSeq(i);
-            boardVO.setTitle("제목" + i);
-            boardVO.setWriter("테스터");
-            boardVO.setContent(i + "번 내용입니다.");
-            boardVO.setCreateDate(new Date());
-            boardVO.setCnt(0);
-            boardVOArrayList.add(boardVO);
+            BoardDto boardDto = new BoardDto();
+            boardDto.setSeq(i);
+            boardDto.setTitle("제목" + i);
+            boardDto.setWriter("테스터");
+            boardDto.setContent(i + "번 내용입니다.");
+            boardDto.setCreateDate(new Date());
+            boardDto.setCnt(0);
+            boardDtoArrayList.add(boardDto);
         }
-        return boardVOArrayList;
+        return boardDtoArrayList;
     }
 
     @GetMapping("/home/getBoardList5")
-    public List<BoardVO> getBoardList1() {
-        List<BoardVO> boardVOArrayList = new ArrayList<>();
+    public List<BoardDto> getBoardList1() {
+        List<BoardDto> boardDtoArrayList = new ArrayList<>();
         for(int i = 1; i <= 10; i++) {
-            BoardVO boardVO = new BoardVO();
-            boardVO.setSeq(i);
-            boardVO.setTitle("제목" + i);
-            boardVO.setWriter("테스터");
-            boardVO.setContent(i + "번 내용입니다.");
-            boardVO.setCreateDate(new Date());
-            boardVO.setCnt(0);
-            boardVOArrayList.add(boardVO);
+            BoardDto boardDto = new BoardDto();
+            boardDto.setSeq(i);
+            boardDto.setTitle("제목" + i);
+            boardDto.setWriter("테스터");
+            boardDto.setContent(i + "번 내용입니다.");
+            boardDto.setCreateDate(new Date());
+            boardDto.setCnt(0);
+            boardDtoArrayList.add(boardDto);
         }
-        return boardVOArrayList;
+        return boardDtoArrayList;
     }
 
 
