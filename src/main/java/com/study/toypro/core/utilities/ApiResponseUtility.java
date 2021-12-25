48d04832d18ba02b239c72bc6122ce8b7880f972
inclutter <augustman@knou.ac.kr>
2021-12-25 23:10:08 +0900

    Response format was modified from text to json.

--- src/main/java/com/study/toypro/core/utilities/ApiResponseUtility.java
+++ src/main/java/com/study/toypro/core/utilities/ApiResponseUtility.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 
 public class ApiResponseUtility {
     public static <T> ResponseEntity<Object> success(T data) {
-        final ApiResult<T> body = new ApiResult<T>("0200","0200",data);
-        return new ResponseEntity<Object>(body, HttpStatus.OK);
+        final ApiResult<T> body = new ApiResult<>("0200","0200",data);
+        return new ResponseEntity<>(body, HttpStatus.OK);
     }
 }
