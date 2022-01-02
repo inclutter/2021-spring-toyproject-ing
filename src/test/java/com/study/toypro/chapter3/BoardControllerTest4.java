613d0d231ef30fce44fb04b4cfa854bc70249223
inclutter <augustman@knou.ac.kr>
2022-01-02 23:22:16 +0900

    Modified about junit controller test.

--- src/test/java/com/study/toypro/chapter3/BoardControllerTest4.java
+++ src/test/java/com/study/toypro/chapter3/BoardControllerTest4.java
@@ -1,29 +1,42 @@
 package com.study.toypro.chapter3;
 
+import com.study.toypro.api.board.controller.BoardController;
 import com.study.toypro.api.board.controller.BoardTestController;
+import com.study.toypro.api.board.service.BoardService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = BoardTestController.class)
+//@WebMvcTest(controllers = BoardController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class BoardControllerTest4 {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private BoardService boardService;
+
     @Test
     public void testHello() throws Exception {
-        mvc.perform(get("/hello1").param("name", "둘리"))
+        when(boardService.hello("둘리")).thenReturn("Hello : 둘리");
+        when(boardService.hello("볼리")).thenReturn("Bye : 볼리");
+        mvc.perform(get("/hello").param("name", "둘리"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello : 둘리"))
+                .andExpect(content().string("Hello : 볼리"))
                 .andDo(print());
     }
 }
